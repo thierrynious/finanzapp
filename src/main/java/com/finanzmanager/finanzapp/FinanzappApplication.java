691e7d859058499111e03f84a44f82818aac5c43
Thierrynious <thierrynious@hotmail.fr>
2026-01-29 20:39:26 +0100

    Initial commit – Spring Boot Basics

---
+++ src/main/java/com/finanzmanager/finanzapp/FinanzappApplication.java
@@ -0,0 +1,13 @@
+package com.finanzmanager.finanzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzappApplication.class, args);
+	}
+
+}
